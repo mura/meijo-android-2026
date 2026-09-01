28a97fc7a199c3f2b1b64952348407cef5bb90db
Yohei Murayama <muracchi@gmail.com>
2026-09-01 12:16:33 +0900

    TextViewとImageViewの演習

--- app/src/main/java/com/example/meijo_android_2026/MainActivity.java
+++ app/src/main/java/com/example/meijo_android_2026/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.meijo_android_2026;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +9,31 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.meijo_android_2026.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //TextView textView = findViewById(R.id.text_view);
+        //textView.setText(R.string.text2);
+        // text_view → textView
+        binding.textView.setText(R.string.text2);
+        binding.imageView.setImageResource(R.drawable.ic_add_location);
     }
 }
